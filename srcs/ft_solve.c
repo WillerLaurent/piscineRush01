9bc423e446d00c8c25611f3a8b83377c58485858
Gregory Artaud <gartaud@z1r8p5.42lyon.fr>
2020-09-20 16:26:52 +0200

    swag

--- srcs/ft_solve.c
+++ srcs/ft_solve.c
@@ -7,13 +7,13 @@ int		ft_is_move_valid(char **tab, int i, int j, int value)
 	int	y;
 	int count;
 
-	printf("=== ENTREE IS_VALID ===\n\n");
+//	printf("=== ENTREE IS_VALID ===\n\n");
 	if (tab[i][j] != 0 || value <= 0 || value > N ||
 		i <= 0 || i >= N + 1 || j <= 0 || j >= N + 1)
 		return (0);
-	printf("\tTests de base passés !\n");
+//	printf("\tTests de base passés !\n");
 	tab[i][j] = value;
-
+/*
 	printf("\tcount_rl = %d, tab[%d][0] = %d\n", ft_count_rowleft(tab, i), i, tab[i][0]);
 	printf("\tcount_rr = %d, tab[%d][%d] = %d\n", ft_count_rowright(tab, i), i, N + 1, tab[i][N + 1]);
 	printf("\tcount_cu = %d, tab[0][%d] = %d\n", ft_count_colup(tab, j), j, tab[0][j]);
@@ -24,12 +24,9 @@ int		ft_is_move_valid(char **tab, int i, int j, int value)
 	printf("\tcond rr = %d\n", (ft_count_rowright(tab, i) <= tab[i][N + 1]));
 	printf("\tcond cu = %d\n", (ft_count_colup(tab, j) <= tab[0][j]));
 	printf("\tcond cd = %d\n", (ft_count_coldown(tab, j) <= tab[N + 1][j]));
-
-	res = ((ft_count_rowleft(tab, i) <= tab[i][0]) &&
-			(ft_count_rowright(tab, i) <= tab[i][N + 1]) &&
-			(ft_count_colup(tab, j) <= tab[0][j]) &&
-			(ft_count_coldown(tab, j) <= tab[N + 1][j]));
-	printf("\tTests de count passés ! (res = %d)\n", res);
+*/
+//	printf("\tTests de count passés ! (res = %d)\n", res);
+	res = 1;
 	y = 0;
 	count = 0;
 	while (++y < N + 1)
@@ -56,6 +53,24 @@ int		ft_is_move_valid(char **tab, int i, int j, int value)
 	return (res);
 }
 
+int		ft_is_solution(char **tab)
+{
+	int	i;
+	int	res;
+
+	i = 0;
+	res = 1;
+	while (res && ++i <= N)
+	{
+		res = res &&
+			((ft_count_rowleft(tab, i) <= tab[i][0]) &&
+			(ft_count_rowright(tab, i) <= tab[i][N + 1]) &&
+			(ft_count_colup(tab, i) <= tab[0][i]) &&
+			(ft_count_coldown(tab, i) <= tab[N + 1][i]));
+	}
+	return (res);
+}
+
 void	ft_next_empty_space(char **tab, int *i, int *j)
 {
 	int	x;
@@ -74,6 +89,8 @@ void	ft_next_empty_space(char **tab, int *i, int *j)
 				return ;
 			}
 	}
+	*i = -1;
+	*j = -1;
 	return ;
 }
 
@@ -86,22 +103,18 @@ int		ft_solve(char **tab, int move_left)
 	//printf("\n\n");
 	//ft_putsol(tab);
 	if (move_left == 0)
-	{
-		//ft_putsol(tab);
-		printf("J'AI TROUVE UNE SOLUTION !!! :))))\n\n");
-		return (1);
-	}
+		return (ft_is_solution(tab));
 	ft_next_empty_space(tab, &i, &j);
 //	printf("(i, j) = (%d, %d)\n", i, j);
 	next_move = 0;
 	while (++next_move <= N)
 	{
-		printf("On va tenter : %d\n", next_move);
-		printf("\n");
+//		printf("On va tenter : %d\n", next_move);
+//		printf("\n");
 		if (ft_is_move_valid(tab, i, j, next_move))
 		{
 			tab[i][j] = next_move;
-			ft_putsol(tab);
+//			ft_putsol(tab);
 			if (ft_solve(tab, move_left - 1))
 				return (1);
 			tab[i][j] = 0;
