670caffe9c305fa8d7db5992dff564aea10cfc9a
Laurent Willer <lwiller@z1r4p5.42lyon.fr>
2020-09-20 15:16:30 +0200

    ft_init new version

--- srcs/ft_init.c
+++ srcs/ft_init.c
@@ -6,7 +6,7 @@
 /*   By: lwiller <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/19 16:29:06 by lwiller           #+#    #+#             */
-/*   Updated: 2020/09/19 17:33:56 by lwiller          ###   ########lyon.fr   */
+/*   Updated: 2020/09/20 14:28:27 by lwiller          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	ft_check_input(char *str)
 
 	nb = ft_atoi(str);
 	if (nb <= 0 || nb > N)
-		return (0);
+		ft_putstr("Error");
 	return (nb);
 }
 
@@ -31,27 +31,21 @@ int		ft_init_raw(char **tab, int count, char **argv)
 	x = 0;
 	y = 0;
 	str = argv[1];
-	while (count < 15)
+	while (count < N * 2)
 	{
-		//remplissage ligne du haut
 		while (x < N)
 		{
-			while (str[count] == 32)
-				count++;
-			tab[y][x+1] = ft_check_input(str + count);
-			count++;
+			tab[y][x + 1] = ft_check_input(str + count);
+			count += 2;
 			x++;
 		}
-		//remplissage ligne du bas
 		x = 0;
 		y = N + 1;
 		while (x < N)
 		{
-			while (str[count] == 32)
-				count++;
-			tab[y][x+1] = ft_check_input(str + count);
+			tab[y][x + 1] = ft_check_input(str + count);
 			x++;
-			count++;
+			count += 2;
 		}
 	}
 	return (count);
@@ -62,44 +56,55 @@ int		ft_init_colon(char **tab, int count, char **argv)
 	int		x;
 	int		y;
 	char	*str;
-	
+
 	x = 0;
 	y = 0;
 	str = argv[1];
-	while (str[count])
+	while (count < N * 6)
 	{
-		//remplissage cote gauche
-		x = 0;
-		y = 0;
 		while (y < N)
 		{
-			while (str[count] == ' ')
-				count++;
 			tab[y + 1][x] = ft_check_input(str + count);
 			y++;
-			count++;
+			count += 2;
 		}
-		//remplissage cote droit
 		x = N + 1;
 		y = 0;
 		while (y < N)
 		{
-			while (str[count] == ' ')
-				count++;
 			tab[y + 1][x] = ft_check_input(str + count);
 			y++;
-			count++;
+			count += 2;
 		}
 	}
 	return (count);
 }
 
-char	**ft_init(int argc, char **argv)
+void	ft_init_array_box(char **tab)
 {
-	char **tab;
 	int x;
 	int y;
-	int count;
+
+	x = 1;
+	y = 1;
+	while (y < N + 1)
+	{
+		x = 1;
+		while (x < N + 1)
+		{
+			tab[y][x] = 0;
+			x++;
+		}
+		y++;
+	}
+}
+
+char	**ft_init(int argc, char **argv)
+{
+	char	**tab;
+	int		x;
+	int		y;
+	int		count;
 
 	if (argc != 2)
 		return (0);
@@ -113,13 +118,6 @@ char	**ft_init(int argc, char **argv)
 	count = 0;
 	count = ft_init_raw(tab, count, argv);
 	count = ft_init_colon(tab, count, argv);
-	//initialisation des cases a 0
-	y = 0;
-	while (++y < N + 1)
-	{
-		x = 0;
-		while (++x < N + 1)
-			tab[y][x] = 0;
-	}
+	ft_init_array_box(tab);
 	return (tab);
 }
